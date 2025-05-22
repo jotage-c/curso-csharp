beb21d44f58c1b8f96a37822316ff2502e7efeb3
jotage-c <maninhodoisdois25@gmail.com>
2025-05-22 20:43:36 -0300

    .

---
+++ Aula11-ExcecaoArquivoNaoAchado/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula11_ExcecaoArquivoNaoAchado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader leitor = new StreamReader(@"C:\Users\first informatica\Desktop\curso csh\Csharp\CursoCSHARP\Aula11-ExcecaoArquivoNaoAchado\dados.txt"))
+                {
+                    string conteudo = leitor.ReadToEnd();
+                    Console.WriteLine("Conteúdo do arquivo\n" + conteudo);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Arquivo não encontrado");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro de Leitura: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Aula11-Excecoes/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Aula11_Excecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3 };
+            //Console.WriteLine(num[3]); // ArrayIndexOutOfRangeException
+
+            int x = 10;
+            int y = 0;
+            //Console.WriteLine(x / y); // DivideByZeroException
+
+            try
+            {
+                Console.Write("Digite um número inteiro: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Digite outro número inteiro: ");
+                int b = int.Parse(Console.ReadLine());
+
+                int resultado = a / b;
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Erro: Não é possível dividir por zero.");
+                Console.WriteLine(ex.Message);
+                Console.Write("Digite mais um número: ");
+                int c = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Resultado: {10/c}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Levantou o erro: " + ex.Message);
+            }
+        }
+    }
+}
