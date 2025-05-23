57aeb46d96232d1d41aa30af7af6e9c076b3e3f5
jotage-c <maninhodoisdois25@gmail.com>
2025-05-22 21:00:28 -0300

    .

---
+++ Aula11-ExcecaoCustomizada/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Channels;
+
+namespace Aula11_ExcecaoCustomizada
+{
+    public class SenhaInvalidaException : Exception
+    {
+        public SenhaInvalidaException(string mensagem)
+            : base(mensagem) { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite a senha: ");
+                string senha = Console.ReadLine();
+                ValidarSenha(senha);
+            }
+            catch (SenhaInvalidaException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+            void ValidarSenha(string senha)
+            {
+                if (senha.Length < 6)
+                    throw new SenhaInvalidaException("senha deve ter 6 digitos no minimo");
+
+            }
+        }
+    }
+}
